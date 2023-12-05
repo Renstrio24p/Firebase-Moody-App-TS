83355a7764f2de72747bd50c53f89152f96e91ce
Waren Gador <waren.gador15@gmail.com>
2023-12-06 04:02:42 +0800

    update

--- src/assets/firebase/DatabaseConfig.ts
+++ src/assets/firebase/DatabaseConfig.ts
@@ -4,12 +4,12 @@ import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 import { FirebaseTypes } from "./FirebaseTypes";
 
 const firebaseConfig: FirebaseTypes = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyBMBJcoGlV6Df4ySDHTaxcMwDacVl48RKA",
+  authDomain: "moddy-d06db.firebaseapp.com",
+  projectId: "moddy-d06db",
+  storageBucket: "moddy-d06db.appspot.com",
+  messagingSenderId: "957833856518",
+  appId: "1:957833856518:web:27c48fb939bd1dc2562587"
 };
 
 // Initialize Firebase
